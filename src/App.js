7b8935c52744559949dfc4b17077b9fe88ce30ce
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-12-28 21:30:06 +0100

    not displaying data comming from the API, problem with hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
+import React ,{useState, useEffect}from 'react';
 import './App.css';
 import Weather from './components/weather';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "weather-icons/css/weather-icons.css";
 
 
-function App() {
+//const API_key = "b5014333f2554516a7172c05dbe90c57";
+
+const App = ()=> {
+  // settig the initial state
+  const [city, setCity] = useState({});
+
+
+  // making the API call
+
+  async function fetchWeather() {
+    const weather = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`);
+    const response = await weather.json();
+
+
+    setCity(response);
+  }
+
+  useEffect(() => {
+    fetchWeather();
+  },[]);
+
+
   return (
     <div className="App">
-      <h1>Deux freres</h1>
-      <weather/>
+      <h1>Weather app</h1>
+      <Weather/>
     </div>
   );
 }
--- src/components/weather.js
+++ src/components/weather.js
@@ -4,11 +4,32 @@ const Weather = () => {
   return(
     <div className="container">
       <div className="cards">
-      <h1>London</h1>
+      <h1>Paris</h1>
+      <h5 className="py-4">
+        {/* check dosc for more info*/}
+        <i className="wi wi-day-sunny display-1"></i>
+
+      </h5>
+
+      <h1 className="py-2">25&deg;</h1>
+      {/*  show min and max temp*/}
+      {minmaxTemp(24,19)}
+
+      <h4 className="py-3">raining</h4>
+
       </div>
     </div>
     );
 
+}
+
+function minmaxTemp(min,max) {
+  return(
+    <h3>
+      <span className="px-4">{min}&deg;</span>
+      <span className="px-4">{max}&deg;</span>
+    </h3>
+    );
 }
 
 export default Weather;
