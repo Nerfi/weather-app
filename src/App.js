8010405df681e3f13e9ff49d931e7c75deaaf7e0
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-12-28 18:58:34 +0100

    boostrap add c

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import './App.css';
+import Weather from './components/weather';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Deux freres</h1>
+      <weather/>
     </div>
   );
 }
