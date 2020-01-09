c689a3cd85b7988ae55a38006a7d8748ea5c7cd9
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-01-09 19:50:28 +0100

    not fucking working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import React ,{useState, useEffect, useReducer}from 'react';
+import React ,{useState, useEffect}from 'react';
 import './App.css';
 import Weather from './components/weather';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,56 +7,44 @@ import "weather-icons/css/weather-icons.css";
 import Form from './components/form';
 
 
+function App (props) {
+
 const API_key = "b5014333f2554516a7172c05dbe90c57";
 
-const icons = {
-  Thunderstorm: "wi-thunderstorm",
+const [fields, setFields] = useState({city: '', country: ''});
+
+const [icon, setIcon] = useState({
+      Thunderstorm: "wi-thunderstorm",
       Drizzle: "wi-sleet",
       Rain: "wi-storm-showers",
       Snow: "wi-snow",
       Atmosphere: "wi-fog",
       Clear: "wi-day-sunny",
       Clouds: "wi-day-fog"
-}
+ });
 
-//new state
-const initialState = {city: '', country: '', icon: '' };
-// new code
-const reducer = (state, action) => {
-  switch(action.type) {
-    case 'cahnge_city':
-    return{...state, city: action.type};
-    default:
-    return state;
-  }
-
-}
-
-function App (props) {
-
-const [state, dispatch] = useReducer(reducer, initialState);
-
-  async function fetchData (e){
+  async function fetchData (){
    // e.preventDefault();
     const data = await fetch(`api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`);
     const response = await data.json();
-    dispatch({type: 'response_api', data: response.data});
-  console.log(response.data);
+    setFields(response);
+  console.log(response);
 
   }
 
 useEffect(() => {
+  fetchData();
 
 },[]);
 
   return(
     <div className="App">
     <h1>Weather app</h1>
-    <Form/>
+    <Form loadweather={""}/>
 
     <Weather
-    city={state.city}
-    country={state.country}
+    city={fields.name}
+    country={fields.country}
     />
 
     </div>
